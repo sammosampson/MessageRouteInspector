4a753a82a9ac590ae9a7b6b3b24e717a165b5b73
sammosampson <sammosampson@gmail.com>
2015-12-31 15:32:02 +0000

    Made client have url passed in

--- Client/Bootstrapping/BootstrapBuilderConfigurationExtensions.cs
+++ Client/Bootstrapping/BootstrapBuilderConfigurationExtensions.cs
@@ -4,10 +4,10 @@
 
     public static class BootstrapBuilderConfigurationExtensions
     {
-        public static BootstrapBuilderConfiguration SetupMessageRouteInspectorClient(this BootstrapBuilderConfiguration config)
+        public static BootstrapBuilderConfiguration SetupMessageRouteInspectorClient(this BootstrapBuilderConfiguration config, string serverUrl)
         {
             return config
-                .RegisterBuildAction(c => c.RegisterInstance(() => GraphQlServerUri.Parse("http://localhost:8081")))
+                .RegisterBuildAction(c => c.RegisterInstance(() => GraphQlServerUri.Parse(serverUrl)))
                 .RegisterBuildAction(c => c.RegisterInstance<IGraphQlServer, GraphQlServer>())
                 .RegisterBuildAction(c => c.RegisterInstance<IMessageLoggingClient, MessageLoggingClient>());
         }
